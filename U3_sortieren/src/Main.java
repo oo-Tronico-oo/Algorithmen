17094e31101a0a3bbd0c9c41fcdf8b3b263a635f
oo-Tronico-oo <tronico@o2online.de>
2016-10-27 15:30:20 +0200

    first commit

---
+++ U3_sortieren/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * @Modul: Algorithmen
+ * @Dozentin: Prof. Dr. H. Ripphausen-Lipa Beuth Hochschule für Technik
+ * @Uebungsaufgabe: Implementierung des Sortierverfahren ...??????????????...
+ * @author Lisa Bitterling, Katrin Schulze, Nico Nauendorf
+ * @erstellt am 27. Oktober 2016
+ */
+public class Main {
+
+    /**
+     * Methode zum starten der Anwendung
+     * @param args Array von Parametern, die beim start der Anwendung übergeben wurden
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ U3_sortieren/src/Sortierverfahren.java
@@ -0,0 +1,37 @@
+
+import java.util.List;
+
+/**
+ * @Modul: Algorithmen
+ * @Dozentin: Prof. Dr. H. Ripphausen-Lipa Beuth Hochschule für Technik
+ * @Uebungsaufgabe: Implementierung des Sortierverfahren ...??????????????...
+ * @author Lisa Bitterling, Katrin Schulze, Nico Nauendorf
+ * @erstellt am 27. Oktober 2016
+ */
+
+/**
+ * Dies ist eine Serviceklasse, die eine beliebige Menge von Integerzahlen sortiert
+ * aktuelle Version sortiert nur Integerzahlen ist aber (was Bezeichnungen angeht)
+ * in teilen forbereitet fuer beliebige Objekte
+ */
+public class Sortierverfahren {
+    
+    /**
+     * Konstruktor der Klasse wurde als privat deklariert, da es eine statische
+     * Klasse ist und keine Instanz notwendig ist.
+     */
+    private Sortierverfahren(){
+    }
+    
+    /**
+     * statische Methode zum sortieren von Objekten (hier Integerzahlen) nach
+     * dem Sortierverfahren ....????????....
+     * 
+     * @param objektMenge erwartet eine unsortierte Menge von Objekten vom Typ List
+     * @return gibt eine sortierte Menge von Objekten als List zurueck
+     */
+    static List sortiereListe(List objektMenge){
+        
+        return null;
+    }
+}
