5ac804ccaa2e8cdc5671722e2105a3eb667da13f
oo-Tronico-oo <tronico@o2online.de>
2016-11-10 12:58:22 +0100

    Sortierverfahren Mergesort vollständig implementiert

--- U3_sortieren/src/Main.java
+++ U3_sortieren/src/Main.java
@@ -20,7 +20,9 @@ public class Main {
     public static void main(String[] args) {
         //lese die Datei ein und generiere ein int[]
         int[] ausgangsArray = FileIntArray.FileToIntArray("src/DateinIntZahlen/Rand10_1");
-        Sortierverfahren.sortiereNachMergeSort(ausgangsArray, 0, ausgangsArray.length - 1);
+        Sortierverfahren.sortiereNachMergeSort(ausgangsArray);
+        gebeArrayInKonsoleAus(ausgangsArray);
+        pruefeObRichtigSortiert(ausgangsArray);
     }
 
     /**
@@ -38,5 +40,33 @@ public class Main {
         }
         
         return intList;
+    }
+    
+    /**
+     * private Hilfsmethode um ein Integerarray auszugeben
+     * @param array erwartet ein intArray
+     */
+    private static void gebeArrayInKonsoleAus(int[] array){
+        System.out.print("\nsortiert: [" + array[0]);
+        for(int i = 1; i < array.length; i++){
+            System.out.print(", " + array[i]);
+        }
+        System.out.println("]");
+    }
+
+    /**
+     * private Hilfsmethode um die richtige Reihenfolge zu prüfen und ein OK auf der Konsole auszugeben
+     * @param array erwartet ein sortiertes intArray
+     */
+    private static void pruefeObRichtigSortiert(int[] array) {
+        boolean pruefung = true;
+        //überprüft jedes Element im array ob es kleiner als das Element rechts daneben ist
+        for(int i = 0; i < array.length - 1; i++){
+            if(array[i] > array[i+1]){
+                pruefung = false;
+                break;
+            }
+        }
+        System.out.println("Sortierung ist " + ((pruefung)?"richtig":"falsch") + "!");
     }
 }
--- U3_sortieren/src/Sortierverfahren.java
+++ U3_sortieren/src/Sortierverfahren.java
@@ -19,23 +19,39 @@ public class Sortierverfahren {
     }
     
     /**
-     * statische Methode zum sortieren von Integerzahlen nach
-     * dem Sortierverfahren Mergesort innerhalb der linken und rechten Grenze
+     * statische Methode zum sortieren von Integerzahlen nach dem Sortierverfahren Mergesort
      * 
      * @param objektMenge erwartet eine unsortierte Menge von Integerzahlen vom Typ int[]
      * @return gibt eine sortierte Menge von Integerzahlen als int[] zurueck
      */
-    static int[] sortiereNachMergeSort(int[] objektMenge, int linkeGrenze, int rechteGrenze){
+    public static int[] sortiereNachMergeSort(int[] objektMenge){
+        //wenn objektMenge <= 1 oder null ist, gib die Menge bzw. null sofort zurück
+        if(objektMenge == null || objektMenge.length <= 1) return objektMenge;
+        else return sortiereNachMergeSortRekursiv(objektMenge, 0, objektMenge.length - 1);
+    }
+    
+    /**
+     * private statische Methode zum sortieren von Integerzahlen nach
+     * dem Sortierverfahren Mergesort innerhalb der linken und rechten Grenze
+     * 
+     * @param objektMenge erwartet eine unsortierte Menge von Integerzahlen vom Typ int[]
+     * @param linkeGrenze erwartet die Indexzahl der linken Grenze von der Menge
+     * @param rechteGrenze erwartet die Indexzahl der rechten Grnze von der Menge
+     * @return gibt eine sortierte Menge von Integerzahlen als int[] zurueck
+     */
+    private static int[] sortiereNachMergeSortRekursiv(int[] objektMenge, int linkeGrenze, int rechteGrenze){
+        //wenn objektMenge <= 1 gib die Menge sofort zurück
+        if((rechteGrenze - linkeGrenze) < 1) return objektMenge;
         //Zerlege n-elementige Teilfolge A in zwei Teilfolgen A1, A2 der Länge n/2
-        int mitte = 0;
+        int mitte = (rechteGrenze - linkeGrenze) / 2 + linkeGrenze;
         //Sortiere die zwei Teilfolgen rekursiv durch Merge-Sort; erhalte A´1, A´2
         //sortiere linke Haelfte
         if(linkeGrenze < mitte){
-            sortiereNachMergeSort(objektMenge, linkeGrenze, mitte);
+            sortiereNachMergeSortRekursiv(objektMenge, linkeGrenze, mitte);
         }
         //sortiere rechte Haelfte
         if((mitte + 1) < rechteGrenze){
-            sortiereNachMergeSort(objektMenge, mitte + 1, rechteGrenze);
+            sortiereNachMergeSortRekursiv(objektMenge, mitte + 1, rechteGrenze);
         }  
         //Mische die beiden sortierten Teilfolgen A´1 und A´2 zu einer sortierten Gesamtfolge A´
         merge(objektMenge, linkeGrenze, mitte, rechteGrenze);
@@ -52,20 +68,54 @@ public class Sortierverfahren {
      * @param rechteGrenze ist die rechte Grenze der Teilfolge
      */
     private static void merge(int[] objektMenge, int linkeGrenze, int mitte, int rechteGrenze) {
-        // Teilfeld A1 geht von Index l bis m
-        // Teilfeld A2 geht von Index m+1 bis r
-        // Precondition: A1 ist sortiert und A2 ist sortiert
-        // H sei Hilfsfeld
-        
-        // l aktueller Index A1, m+1 aktueller Index A2
-
+        // Sicherung des Wertes der linken Grenze
+        int backupLinkeGrenze = linkeGrenze;
+        // Teilfeld A1 geht von Index linkeGrenze bis mitte
+        // Teilfeld A2 geht von Index mitte+1 bis rechteGrenze
+        // Vorbedingung: A1 ist sortiert und A2 ist sortiert
+        // tempArray sei Hilfsfeld
+        int[] tempArray = new int[(rechteGrenze - linkeGrenze) + 1];
+        // linkeGrenze aktueller Index A1, mitte+1 aktueller Index A2
+        int pointerA1 = linkeGrenze;
+        int pointerA2 = mitte + 1;
+        // speichert den zuletzt beschriebenen Index im tempArray
+        int pointerTempArray = 0;
         // while (weder A1 noch A2 ist vollständig von links nach
         // rechts durchlaufen)
-        // kopiere kleineren aktuellen Wert von A1, A2 nach H
-        // erhöhe akt. Index von Teilfeld mit kleinerem Wert
-        // erhöhe aktuellen Index von H
-        // übertrage Werte des noch nicht vollständig
-        // durchlaufenen Teilfelds nach H
-        // kopiere H nach A (von Index l bis Index r)
+        while(pointerA1 <= mitte && pointerA2 <= rechteGrenze ){
+            // kopiere kleineren aktuellen Wert von A1, A2 nach tempArray
+            if(objektMenge[pointerA1] < objektMenge[pointerA2]){
+                tempArray[pointerTempArray] = objektMenge[pointerA1];
+                // erhöhe akt. Index von Teilfeld mit kleinerem Wert
+                pointerA1++;
+            } else{
+                tempArray[pointerTempArray] = objektMenge[pointerA2];
+                // erhöhe akt. Index von Teilfeld mit kleinerem Wert
+                pointerA2++;
+            }
+            // erhöhe aktuellen Index von tempArray
+            pointerTempArray++;
+        }
+        // übertrage Werte des noch nicht vollständig durchlaufenen Teilfelds A1 nach tempArray
+        while(pointerA1 <= mitte){
+            tempArray[pointerTempArray] = objektMenge[pointerA1];
+            // erhöhe akt. Index von Teilfeld A1
+            pointerA1++;
+            // erhöhe aktuellen Index von tempArray
+            pointerTempArray++;
+        }
+        // übertrage Werte des noch nicht vollständig durchlaufenen Teilfelds A2 nach tempArray
+        while(pointerA2 <= rechteGrenze){
+            tempArray[pointerTempArray] = objektMenge[pointerA2];
+            // erhöhe akt. Index von Teilfeld A2
+            pointerA2++;
+            // erhöhe aktuellen Index von tempArray
+            pointerTempArray++;
+        }
+        // kopiere tempArray nach A (von Index backupLinkeGrenze bis Index rechteGrenze)
+        for(int i : tempArray){
+            objektMenge[backupLinkeGrenze] = i;
+            backupLinkeGrenze++;
+        }
     }
 }
